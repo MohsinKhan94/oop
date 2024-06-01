4f624564f8e9e9847b96352bd123ec1859829ca9
Muhammad Mohsin Khan <159677872+MohsinKhan94@users.noreply.github.com>
2024-06-01 22:19:33 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,70 @@
+import inquirer from "inquirer";
+
+class Student {
+    name: string;
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    talk() {
+        console.log(`I am ${this.name} and I am good.`);
+    }
+}
+
+class Person {
+    students: Student[] = [];
+
+    addStudent(obj: Student) {
+        this.students.push(obj);
+    }
+}
+
+const persons = new Person();
+
+const programStart = async (persons: Person) => {
+    console.log("Welcome Guest");
+
+    const ans = await inquirer.prompt({
+        name: "select",
+        type: "list",
+        message: "Who would you like to talk to?",
+        choices: ["self", "students"]
+    });
+
+    if (ans.select === "self") {
+        console.log("I am talking to myself.");
+        console.log("I am feeling fine.");
+    } else if (ans.select === "students") {
+        if (persons.students.length === 0) {
+            console.log("There are no students to talk to.");
+            return;
+        }
+
+        const studentAns = await inquirer.prompt({
+            name: "student",
+            type: "list",
+            message: "Which student would you like to talk to?",
+            choices: persons.students.map(student => student.name)
+        });
+
+        const student = persons.students.find(val => val.name === studentAns.student);
+
+        if (student) {
+            student.talk();
+        } else {
+            console.log("Student not found.");
+        }
+    }
+};
+
+// Example usage:
+const start = async () => {
+    // Adding some sample students
+    persons.addStudent(new Student("Ali"));
+    persons.addStudent(new Student("Abdullah"));
+    persons.addStudent(new Student("wasif"));
+
+    await programStart(persons);
+};
+
+start();
